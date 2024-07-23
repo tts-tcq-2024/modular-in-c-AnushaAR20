5f384a221edb588d60fb74b71aafa8998d8fa234
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-23 16:49:25 +0530

    Update Colour_Sets.c

--- Colour_Sets.c
+++ Colour_Sets.c
@@ -1,5 +1,4 @@
-#ifndef COLOR_VARIABLES_H
-#define COLOR_VARIABLES_H
+#include<colour_sets.h>
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
@@ -7,8 +6,6 @@ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
