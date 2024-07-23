b5d8f63410db6dab54683a9cfffacc747510a0e9
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-23 17:56:09 +0530

    Update Colour_Pair_To_String_Convert.c

--- Colour_Pair_To_String_Convert.c
+++ Colour_Pair_To_String_Convert.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <colour_sets.h>
+#include <colour_set.h>
 #inlcude <colour_pair_to_string_convert.h>
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
