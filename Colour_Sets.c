6eaace3e05b411c14ae1221c96ca552c61939e9c
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-23 16:46:36 +0530

    Update and rename Colour_Variables.h to Colour_Sets.c

--- Colour_Sets.c
+++ Colour_Sets.c
@@ -1,18 +1,18 @@
 #ifndef COLOR_VARIABLES_H
 #define COLOR_VARIABLES_H
 
-#include <stdio.h>
-#include <assert.h> 
-
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
