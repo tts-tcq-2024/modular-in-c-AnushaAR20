6683111e70fac19f99f13ac1b782f30877e4b155
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-23 16:57:31 +0530

    Create Get_Color_From_PairNumber.c

---
+++ Get_Color_From_PairNumber.c
@@ -0,0 +1,12 @@
+#include<Colour_Sets.h>
+#include<Get_Color_From_PairNumber.h>
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
