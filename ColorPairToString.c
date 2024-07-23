392f856ce4cedd6d100ab8ad0f596293da535e99
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-23 16:16:20 +0530

    Update and rename Colour.c to ColorPairToString.c

---
+++ ColorPairToString.c
@@ -0,0 +1,7 @@
+#include<Colour_Variables.h>
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
