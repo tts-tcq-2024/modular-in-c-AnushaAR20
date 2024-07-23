d0ce012ce39f3c101072204fc2532d4c33345679
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-23 18:00:32 +0530

    Create colour.h

---
+++ colour.h
@@ -0,0 +1,11 @@
+#ifndef COLOUR_H
+#define COLOUR_H
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+
+#endif
