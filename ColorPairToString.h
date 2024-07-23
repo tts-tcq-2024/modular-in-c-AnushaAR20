835e0979e8421d86aa3c6e2e97a6d96f8444db9a
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-23 16:33:17 +0530

    Update ColorPairToString.h

--- ColorPairToString.h
+++ ColorPairToString.h
@@ -1,5 +1,8 @@
-
+#ifndef COLORPAIRTOSTRING_H
+#define COLORPAIRTOSTRING_H
 #inlcude<Colour_Variables.h>
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
+
+#endif
