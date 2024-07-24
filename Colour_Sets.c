ecf71bdca40ffb452b10ec8c00c3b95a74e896d9
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-24 13:16:07 +0530

    Update Colour_Sets.c

--- Colour_Sets.c
+++ Colour_Sets.c
@@ -1,8 +1,5 @@
 #include "colour.h"
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
