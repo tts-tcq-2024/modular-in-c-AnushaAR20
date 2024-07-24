f65392e891ae3786c52b34da09134360bae969f0
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-24 13:49:09 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <assert.h> 
-#include <colour_sets.h>
-#include <get_color_from_pairnumber.h>
-#include <colour_pair_to_string_convert.h>
+#include "colour.h"
+#include "get_color_from_pairnumber.h"
+#include "colour_pair_to_string_convert.h"
 
 int main() {
     testNumberToPair(4, WHITE, BROWN);
