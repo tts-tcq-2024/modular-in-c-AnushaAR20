c340eb1ead27e97b4e5730c55bbe7d97d02f9aba
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-24 15:45:46 +0530

    Update test_pair_to_number.h

--- test_pair_to_number.h
+++ test_pair_to_number.h
@@ -1,4 +1,7 @@
 #ifndef TEST_PAIR_TO_NUMBER_H
 #define TEST_PAIR_TO_NUMBER_H
 
+#include "colour.h"
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+
 #endif
